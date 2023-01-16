1f44ec0b99b428b36c49871fe3b9b0bb62c3fd57
Wisdom-Eleeza <wisdom.eleeza@amalitech.com>
2023-01-16 11:33:14 +0000

    Passport-Login-System

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const bcrypt = require('bcrypt')
 const passport = require('passport')
 const flash = require('express-flash')
 const session = require('express-session')
+const methodOverride = require('method-override')
 
 //calling our initialize function from the passport-config.js file
 const initializePassport = require('./passport-config.js')
@@ -38,29 +39,30 @@ app.use(session({
 
 app.use(passport.initialize())
 app.use(passport.session())
+app.use(methodOverride('_method'))
 
 //Creating router for login.ejs and register.ejs
-app.get('/', (req, res) =>{
+app.get('/', checkAuthenticated, (req, res) =>{
     res.render('index.ejs', {name: req.user.name})
 })
 
-app.get('/login', (req, res) =>{
+app.get('/login', checkAuthenticated, (req, res) =>{
     res.render('login.ejs')
 })
 
-app.post('/login', passport.authenticate('local', {
+app.post('/login',checkAuthenticated, passport.authenticate('local', {
     successRedirect: '/',
     failureRedirect: '/login',
     failureFlash: true
 })) 
 
-app.get('/register', (req, res) =>{
+app.get('/register', checkAuthenticated, (req, res) =>{
     res.render('register.ejs')
 })
 
 //We are using asynchronous code here,
 //so we use try and catch here
-app.post('/register', async (req, res) =>{
+app.post('/register',checkAuthenticated, async (req, res) =>{
     try{
         const hashedPassword = await bcrypt.hash(req.body.password, 10)
         users.push({
@@ -75,6 +77,27 @@ app.post('/register', async (req, res) =>{
     }
     console.log(users)
 })
+
+    app.delete('/logout', (req, res)=>{
+        req.logOut()
+        res.redirect('/login')
+    })
+
+function checkAuthenticated(req, res, next){
+    if(req.isAuthenticated()){
+        return next()
+    }
+
+    res.redirect('/login')
+}
+
+function checkNotAuthenticated(req, res, next){
+    if(req.isAuthenticated()){
+        return res.redirect('/')
+    }
+    next()
+    
+}
 app.listen(8080)
 
 
